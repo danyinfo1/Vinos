fa6c9401a0a2cb5736b8532abeaafdaa120d39e3
danyinfo1 <danyinfo1@gmail.com>
2014-08-30 14:07:48 -0600

    new proyect

---
+++ Vinos/Vinos/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Vinos
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
